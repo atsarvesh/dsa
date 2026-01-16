b6ff9741ea1643500a64a79ed8ab7dc94580e255
sarvbesh <atsarveshyadav@gmail.com>
2026-01-16 21:15:42 +0530

    patterns done

--- patterns/22_pattern.cpp
+++ patterns/22_pattern.cpp
@@ -1,15 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+    Concentric Square Pattern
+
+    For n = 4
+
+    4 4 4 4 4 4 4
+    4 3 3 3 3 3 4
+    4 3 2 2 2 3 4
+    4 3 2 1 2 3 4
+    4 3 2 2 2 3 4
+    4 3 3 3 3 3 4
+    4 4 4 4 4 4 4
+
+*/
+
 int main(){
 
     int n;
+    cout << "Enter the size of the pattern: ";
     cin >> n;
-    // for(int i = 1; i <= n; i++){
-    //     for(int j = 1; j <= i; j++){
-    //         cout << "*";
-    //     }
-    //     cout << endl;
-    // }
+
+    for(int i = 0; i <= 2*n-2; i++){
+        for(int j = 0; j <= 2*n-2; j++){
+
+            int top = i;
+            int left = j;
+            int right = (2*n-2) - j;
+            int bottom = (2*n-2) - i;
+            
+            int min_dist = min(min(top, bottom), min(left, right));
+            cout << n - min_dist << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
